e7be5f6a32a2a06ae73b683287c806a47da066ce
Joseph <dabigjoe6@gmail.com>
2020-03-21 14:00:24 +0100

    Completed how it spreads screen

--- src/screens/HowItSpreads.js
+++ src/screens/HowItSpreads.js
@@ -1,17 +1,23 @@
-import React from 'react'
+import React, { useRef, useContext } from 'react'
 import { View, Text, Image, Dimensions, ImageBackground } from 'react-native';
+import { Button, Icon } from 'react-native-elements';
 import Carousel from 'react-native-snap-carousel';
+import { theme } from '../context/Theme';
 import { Container } from '../components';
 
 const WINDOW_HEIGHT = Dimensions.get('window').height;
 const WINDOW_WIDTH = Dimensions.get('window').width;
 
-export default function HowItSpreads() {
+export default function HowItSpreads(props) {
+
+	const _carousel = useRef(null);
+
+	const activeTheme = useContext(theme).globalTheme;
 
 	const styles = {
 		overlayContainer: {
 			zIndex: 100,
-			height: '40%',
+			height: '30%',
 			width: '100%',
 			backgroundColor: '#00000090',
 			position: 'absolute',
@@ -19,38 +25,83 @@ export default function HowItSpreads() {
 			padding: 20
 		}
 	}
-	return (
-		<Container>
-			<View style={{ alignItems: 'center', height: WINDOW_HEIGHT - 200, width: 280, alignSelf: 'center' }}>
+
+	function HowItSpreadsCard(props) {
+		return (
+			<View style={{ alignItems: 'center', height: WINDOW_HEIGHT - 150, width: 330, alignSelf: 'center' }}>
 				<Image
-					source={require('../res/background.jpg')}
-					style={{ height: '60%', width: '100%' }}
+					source={props.backgroundImage}
+					style={{ height: '70%', width: '100%' }}
 				/>
 				<Image
-					source={require('../res/background.jpg')}
-					style={{ height: '40%', width: '100%' }}
+					source={props.backgroundImage}
+					style={{ height: '30%', width: '100%' }}
 					blurRadius={50}
 				/>
 				<View style={styles.overlayContainer}>
-					<Text style={{ fontSize: 18, color: 'white' }}>Person-to-person spread</Text>
-					<Text style={{ fontSize: 11, color: 'white' }}>The virus is thought to spread mainly from person-to-person</Text>
+					<Text style={{ fontSize: 18, color: 'white' }}>{props.title}</Text>
+					<Text style={{ fontSize: 11, color: 'white' }}>{props.description}</Text>
 					<View style={{ marginTop: 10 }}>
-						<View style={{ flexDirection: 'row' }}>
-							<Text style={{ color: 'white', marginRight: 10 }}>*</Text>
-							<Text style={{ fontSize: 11, color: 'white' }}>{"Between people who arer in close contact with one another (within about 6 feet"}</Text>
-						</View>
-						<View style={{ flexDirection: 'row' }}>
-							<Text style={{ color: 'white', marginRight: 10 }}>*</Text>
-							<Text style={{ fontSize: 11, color: 'white' }}>{"Through respiratory droplets produced when an infected person coughs or sneezes."}</Text>
-						</View>
+						{props.children}
 					</View>
 				</View>
-				{/* <ImageBackground
-					source={require('../res/background.jpg')}
-					blurRadius={100}
-					style={{ backgroundColor: 'red', width: '100%', height: '50%' }}>
+			</View>
+		)
+	}
 
-				</ImageBackground> */}
+	function renderItem({ item, index }) {
+		return (
+			<HowItSpreadsCard
+				backgroundImage={item.backgroundImage}
+				title={item.title}
+				description={item.description}
+			>
+				{/* {index == 0 &&
+				} */}
+				{item.child}
+			</HowItSpreadsCard>
+		)
+	}
+	return (
+		<Container>
+			<Button
+				icon={
+					<Icon
+						name="arrow-back"
+						type="material"
+						color={activeTheme.textColor.secondary}
+					/>
+				}
+				containerStyle={{ position: 'absolute', top: 20, left: 20, zIndex: 100 }}
+				buttonStyle={{ backgroundColor: activeTheme.darkTheme ? '#00000070' : '#ffffff70', padding: 12 }}
+				onPress={() => props.navigation.goBack()}
+			/>
+			<View style={{ marginTop: 20, }}>
+				<Carousel
+					ref={_carousel}
+					data={[
+						{
+							backgroundImage: require('../res/background-1.jpg'),
+							title: "Person-to-person spread", description: "The virus is thought to spread mainly from person-to-person", child: <>
+								<View style={{ flexDirection: 'row' }}>
+									<Text style={{ color: 'white', marginRight: 10 }}>*</Text>
+									<Text style={{ fontSize: 11, color: 'white' }}>{"Between people who are in close contact with one another (within about 6 feet"}</Text>
+								</View>
+								<View style={{ flexDirection: 'row' }}>
+									<Text style={{ color: 'white', marginRight: 10 }}>*</Text>
+									<Text style={{ fontSize: 11, color: 'white' }}>{"Through respiratory droplets produced when an infected person coughs or sneezes."}</Text>
+								</View>
+							</>
+						},
+						{
+							backgroundImage: require('../res/background-2.jpg'),
+							title: 'Spread from contact with contaminated surfaces or objects',
+							description: 'It may be possible that a person can get COVID-19 by touching a surface or object that has the virus on it and then touching their own mouth, nose, or possibly their eyes, but this is not thought to be the main way the virus spreads.'
+						}]}
+					renderItem={renderItem}
+					sliderWidth={WINDOW_WIDTH}
+					itemWidth={WINDOW_WIDTH}
+				/>
 			</View>
 		</Container>
 	)
